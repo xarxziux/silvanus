1290be05490b0b8e2c5e1c36bc51314c2ef40148
xarxziux <hairy.one@virgin.net>
2017-01-19 02:48:50 +0000

    0.0.2.11: Fixed broken test file syntax and added test with string tree

--- test/test.js
+++ test/test.js
@@ -6,11 +6,9 @@ const silvanus = require ('../src/0_base/silvanus.js');
 
 describe('Binary tree tests', function() {
     
-    describe ('Empty node', function() {
-      
       const emptyTree = silvanus();
       
-      it ('should return a basic empty tree', function() {
+      it ('Should return a basic empty tree', function() {
           
           expect (emptyTree.isEmpty()).to.equal (true);
           expect (emptyTree.contains()).to.equal (false);
@@ -18,10 +16,9 @@ describe('Binary tree tests', function() {
           // expect (emptyTree.left).to.throw (Error);
           // expect (emptyTree.left).to.throw ('Illegal call on an empty node.');
           
-      });
     });
     
-    describe ('Small number node', function() {
+    it ('Should return a number tree', function() {
         
         let numTree = silvanus();
         numTree = numTree.include (4);
@@ -37,16 +34,87 @@ describe('Binary tree tests', function() {
         const numArray = numTree.toArray();
         const numArrayTotal = numArray.reduce (
             (accum, nextItem) => (accum + nextItem));
-
+        
         expect (numTree.toTree()).to.equal (
-              '{{.,1,.},4,{.,15,{{.,17,.},27,{{.,43,{.,54,.}},86,{.,90,.}}}}}');
+              '{{.,1,.},4,{.,15,{{.,17,.},27,{' +
+              '{.,43,{.,54,.}},86,{.,90,.}}}}}');
         expect (numTree.contains (5)).to.equal (false);
         expect (numTree.contains (43)).to.equal (true);
         expect (numArray.length).to.equal (9);
         expect (numArrayTotal).to.equal (337);
         
     });
+    
+    it ('Should return a string tree', function() {
+        
+        const compareStr = (x, y) => {
+            
+            if ((typeof x !== 'string') ||
+                    (typeof x !== 'string'))
+                throw new Error (
+                        'Invalid parameters to compareStr() function.');
+            
+            if (x === y) return 0;
+            if (x < y) return -1;
+            if (x > y) return 1;
+            throw new Error (
+                    'Illegal fall-through case in compareStr() function');
+            
+        };
+        
+        let strTree = silvanus (compareStr);
+        strTree = strTree.include ('a');
+        strTree = strTree.include ('eg');
+        strTree = strTree.include ('gtxd');
+        strTree = strTree.include ('h');
+        strTree = strTree.include ('ttfr');
+        strTree = strTree.include ('e4r');
+        strTree = strTree.include ('vvgba');
+        strTree = strTree.include ('u');
+        strTree = strTree.include ('1q');
+        strTree = strTree.include ('jny');
+        strTree = strTree.include ('Fe');
+        strTree = strTree.include ('7u8');
+        strTree = strTree.include ('j26');
+        strTree = strTree.include ('_6y');
+        
+        const strArray = strTree.toArray();
+        const strArrayTotal = strArray.reduce (
+            (accum, nextItem) => (accum + nextItem));
+        
+        expect (strTree.toTree()).to.equal ('{{.,1q,{{.,7u8,.},Fe,{.,_6y,.}}}' +
+                ',a,{{.,e4r,.},eg,{.,gtxd,{.,h,{{{.,j26,.},jny,.},ttfr,{{.,u' +
+                ',.},vvgba,.}}}}}}');
+        expect (strTree.contains ('rfrd')).to.equal (false);
+        expect (strTree.contains ('ttfr')).to.equal (true);
+        expect (strArray.length).to.equal (14);
+        expect (strArrayTotal).to.equal (
+                '1q7u8Fe_6yae4reggtxdhj26jnyttfruvvgba');
+        
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
